09d988a60826ff911ad1acd9e1064e22e2ce630d
Mack <bob9531@gmail.com>
2024-08-21 03:11:44 +0800

    Update source path

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { fileURLToPath } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/debut-counter/',
   alias: {
-    '@': path.resolve(__dirname, './src'),
+    '@': fileURLToPath(new URL('./src', import.meta.url)),
   },
 })
