7b4c50bb2e4a9fb4579c8e186d70284a92615df6
Mack <bob9531@gmail.com>
2024-08-21 02:00:58 +0800

    Change the dir visibility

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/debut-counter/',
+  alias: {
+    '@': path.resolve(__dirname, './src'),
+  },
 })
